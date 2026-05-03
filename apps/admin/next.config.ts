b70dc49950796e8cdb7216be648e7bfb7f9adf10
Abhinav Singh <abhinav2006singh2@gmail.com>
2026-05-03 13:53:29 +0530

    minor bug fix

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -3,6 +3,9 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname, "../.."),
+  outputFileTracingIncludes: {
+    "/api/**": ["./src/generated/client/*"],
+  },
   experimental: {
     optimizePackageImports: [
       "@zk-whistleblower/shared",
