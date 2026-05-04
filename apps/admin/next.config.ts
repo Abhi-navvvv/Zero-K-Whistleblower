ece7f46e76d1401878fc5341c681ed70095e273d
Abhinav Singh <abhinav2006singh2@gmail.com>
2026-05-04 22:29:33 +0530

    fixed turbopack error

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -14,6 +14,9 @@ const nextConfig: NextConfig = {
   turbopack: {
     resolveAlias: {
       "@react-native-async-storage/async-storage": "",
+      "@zk-whistleblower/db": "../../packages/db/src/index.ts",
+      "@zk-whistleblower/shared": "../../packages/shared/src/index.ts",
+      "@zk-whistleblower/ui": "../../packages/ui/src/index.ts",
     },
   },
   transpilePackages: ["@zk-whistleblower/shared", "@zk-whistleblower/ui", "@zk-whistleblower/db"],
