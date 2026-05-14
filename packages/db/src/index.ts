ff45651bdcf7f9325868c267555666a329bc0d21
Abhinav Singh <abhinav2006singh2@gmail.com>
2026-05-14 05:45:52 +0530

    made joining existing consensus through report id  + created one round per report limit

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,3 @@
 export { prisma } from "./client";
 export { archiveThread, appendMessage, getThreadSummary, listMessages, markThreadMessagesRead, restoreThread } from "./messages";
-export { createConsensusRequest, addAdminVote, computeConsensusResult, buildConsensusCommitment, signCommitment } from "./consensus";
+export { createConsensusRequest, upsertConsensusRequest, findActiveConsensusForReport, addAdminVote, computeConsensusResult, buildConsensusCommitment, signCommitment } from "./consensus";
